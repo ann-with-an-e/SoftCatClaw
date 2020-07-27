2e857f588601a832801d2e5a51d58b0a4cf5ac7b
ann-with-an-e <62963783+ann-with-an-e@users.noreply.github.com>
2020-07-27 08:40:55 -0400

    Update scauto.py

--- scauto.py
+++ scauto.py
@@ -43,7 +43,7 @@ def dbProcessor(path):
 				"operatingSystem": "Android",
 				"priority": 0,
 				"source": "User Generated",
-				"type": "Database",
+				"type": "FILE",
 				"value": path
 		}
 
